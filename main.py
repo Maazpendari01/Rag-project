82c0c2b17b12f282681f8f61c4d3e0287dd20607
maazpendari <240911885+Maazpendari01@users.noreply.github.com>
2025-11-04 17:05:55 +0530

    Fix CORS configuration to allow Vercel frontend

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allow all origins for now (we'll restrict later)
+    allow_origins=origins,  # Allow all origins for now (we'll restrict later)
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
